f8242ab670abf94668747d790b7dbc33c1eb055e
Joseph Chang <niceinho@gmail.com>
2021-10-01 11:45:25 -0400

    fixed time and set header/question fields to null

--- my-dns-client.py
+++ my-dns-client.py
@@ -4,38 +4,40 @@ import socket
 
 hostname = sys.argv[1] # hostname argument given by command line
 message = b"" # the message to send through socket
-start_time = 0 # start time of socket send attempts
-time = 0 # time elapsed in seconds
+data = b"" # the message to receive from socket
+start_time = 0.0 # start time from beginning of socket send attempts
 attempts = 0 # number of times attempted to send message through socket
 
 # Header Fields
-id
-qr
-opcode
-aa
-tc
-rd
-ra
-z
-rcode
-qdcount
-ancount
-nscount
-arcount
+id = None
+qr = None
+opcode = None
+aa = None
+tc = None
+rd = None
+ra = None
+z = None
+rcode = None
+qdcount = None
+ancount = None
+nscount = None
+arcount = None
 
 # Question Fields
-qname
-qtype
-qclass
+qname = None
+qtype = None
+qclass = None
 
 
 udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # initializing socket as UDP (DGRAM)
 udp_host = socket.gethostname() # client hostname
+print (udp_host)
+server = "8.8.8.8" 
 udp_port = 53 # DNS Server port is 53
 print('socket hostname:',udp_host)
 print('socket port:',udp_port)
-start_time = time.time()
-while (attempts <= 3 and time < start_time+5):
+start_time = time.time() # setting start time
+while (attempts <= 3 and time.time() < start_time+5): # within 3 attempts AND less than 5 seconds elapsed
     attempts += 1
     print('socket send attempt number:',attempts)
     udp_socket.sendto(message, (udp_host, udp_port))
@@ -43,4 +45,3 @@ while (attempts <= 3 and time < start_time+5):
         data, server = udp_socket.recvfrom(53)
     except socket.timeout:
         print('Retrying message send')
-        time = time.time()
