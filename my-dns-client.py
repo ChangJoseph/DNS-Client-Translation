0262c452bd342f830aa96e4331f0f49eaf41fe68
Joseph Chang <niceinho@gmail.com>
2021-10-01 19:05:07 -0400

    made message a byte array fixed header parsing

--- my-dns-client.py
+++ my-dns-client.py
@@ -3,7 +3,7 @@ import time
 import socket
 
 cli_hostname = sys.argv[1] # hostname argument given by command line
-message = 0 # the message to send through socket
+message = [] # the message to send through socket
 message_size = 0 # size of message in bits
 data = 0 # the message to receive from socket
 start_time = 0.0 # start time from beginning of socket send attempts
@@ -13,82 +13,50 @@ attempts = 0 # number of times attempted to send message through socket
 print("Preparing DNS query..")
 
 # Header Fields
-header_id = 170 # start with id 0: 16 bits
-message |= header_id
-message_size += 16
+header_id = 1 # start with id 0: 16 bits
+message.append(0x0)
+message.append(0x1)
 header_qr = 0 # 1 bit: 0 = query; 1 = response
-message = message << 1
-message_size += 1
-message |= header_qr
 header_opcode = 0 # 0 for standard: 4 bit
-message = message << 4
-message_size += 4
-message |= header_opcode
 header_aa = 0 # authoritative answer: 1 bit
-message = message << 1
-message_size += 1
-message |= header_aa
 header_tc = 0 # truncation due to long message: 1 bit
-message = message << 1
-message_size += 1
-message |= header_tc
 header_rd = 0 # recursion desired: 1 bit
-message = message << 1
-message_size += 1
-message |= header_rd
+message.append(0x0)
 header_ra = 0 # recursion available: 1 bit
-message = message << 1
-message_size += 1
-message |= header_ra
 header_z = 0 # 3 bit nothing
-message = message << 3
-message_size += 3
-message |= header_qr
 header_rcode = 0 # response code: 4 bit
-message = message << 4
-message_size += 4
-message |= header_rcode
+message.append(0x0)
 header_qdcount = 1 # number of question entries
-message = message << 16
-message_size += 16
-message |= header_qdcount
+message.append(0x1)
 header_ancount = 0 # number of RR in answer section
-message = message << 16
-message_size += 16
-message |= header_ancount
+message.append(0x0)
 header_nscount = 0 # number of NS RR in authority records section
-message = message << 16
-message_size += 16
-message |= header_nscount
+message.append(0x0)
 header_arcount = 0 # number of RR in additional records section
-message = message << 16
-message_size += 16
-message |= header_arcount
+message.append(0x0)
 
 # Question Fields
 # QNAME tokenizing + parsing
 hostname_split = cli_hostname.split(".")
 for i in hostname_split:
-    message = message << 8
     message_size += 8
-    message |= len(i)
+    message.append(len(i))
     
     for j in i:
-        message = message << 8
         message_size += 8
-        message |= ord(j) # ascii value of character
+        message.append(ord(j)) # ascii value of character
             
-    message << 8 # a 0 byte shows that message reached the end of QNAME
+    message.append(0x0) # a 0 byte shows that message reached the end of QNAME
     message_size += 8
 
 question_qtype = 1
-message = message << 16
 message_size += 16
-message |= question_qtype
+message.append(0x0)
+message.append(0x1)
 question_qclass = 0
-message = message << 16
 message_size += 16
-message |= question_qclass
+message.append(0x0)
+message.append(0x1)
 
 print(message)
 
@@ -116,7 +84,7 @@ while (attempts < 3 and time.time() < start_time+5): # within 3 attempts AND les
     attempts += 1
     print("Sending DNS query..:",attempts)
     try:
-        udp_socket.sendto(message.to_bytes(message_size, byteorder='big'), (udp_server, udp_port)) # sends a message to specified server hostname and port
+        udp_socket.sendto(bytes(message), (udp_server, udp_port)) # sends a message to specified server hostname and port
     except socket.error as err:
         print("Remote host rejected connection:",err)
     
@@ -126,18 +94,18 @@ while (attempts < 3 and time.time() < start_time+5): # within 3 attempts AND les
         print("DNS response received (attempt",attempts,"of 3)")
         print("Processing DNS response..")
         header_id = int.from_bytes(data[0:2],'big')
-        header_qr = int.from_bytes(data[2:3],'big')
-        header_opcode = int.from_bytes(data[3:7],'big')
-        header_aa = int.from_bytes(data[7:8],'big')
-        header_tc = int.from_bytes(data[8:9],'big')
-        header_rd = int.from_bytes(data[9:10],'big')
-        header_ra = int.from_bytes(data[10:11],'big')
-        header_z = int.from_bytes(data[11:14],'big')
-        header_rcode = int.from_bytes(data[14:18],'big')
-        header_qdcount = int.from_bytes(data[18:24],'big')
-        header_ancount = int.from_bytes(data[24:40],'big')
-        header_nscount = int.from_bytes(data[40:56],'big')
-        header_arcount = int.from_bytes(data[56:72],'big')
+        header_qr = int.from_bytes(data[2:3],'big') >> 7
+        header_opcode = (int.from_bytes(data[2:3],'big') >> 3) & 15
+        header_aa = (int.from_bytes(data[2:3],'big') >> 2) & 1
+        header_tc = (int.from_bytes(data[2:3],'big') >> 1) & 1
+        header_rd = int.from_bytes(data[3:4],'big') & 1
+        header_ra = int.from_bytes(data[3:4],'big') >> 7
+        header_z = (int.from_bytes(data[3:4],'big') >> 4) & 7
+        header_rcode = int.from_bytes(data[3:4],'big') & 15
+        header_qdcount = int.from_bytes(data[4:6],'big')
+        header_ancount = int.from_bytes(data[6:8],'big')
+        header_nscount = int.from_bytes(data[8:10],'big')
+        header_arcount = int.from_bytes(data[10:12],'big')
 
         print("----------------------------------------------------------------------------")
         print("header.ID =",header_id)
