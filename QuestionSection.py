ad53f3a101c6261dfea59387dfabc6a8abebc465
Joseph Chang <niceinho@gmail.com>
2021-10-01 19:12:12 -0400

    removed HeaderSection.py and QuestionSection.py

--- QuestionSection.py
+++
@@ -1,9 +0,0 @@
-class QuestionSection{
-	def _init_(self, qName, qType):
-		self.qName = qName
-		self.qType = 1
-	def _str_(self):
-		return "Question Material %s, %d"(self.qName, self.qType)
-	def _repr_(self):
-		return str(self)
-}
